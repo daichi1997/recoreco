338eb347c1659bc6ac8feeac64979c8a686f051c
daichi1997 <tagaaan@gmail.com>
2024-11-14 18:08:52 +0900

    プロフィール編集を本人のみに

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :password, 
+            format: { 
+              with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, 
+              message: 'is invalid. Include both letters and numbers' 
+            }
+
   has_many :recos
-  has_many :comments 
+  has_many :comments
 end
